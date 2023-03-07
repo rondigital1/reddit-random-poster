3276e03a39ebbe8a36b38bd7f9f9fea5ca388c13
Ron <ron@Rons-MacBook-Pro.local>
2023-03-07 17:32:35 -0500

    Edit style of categories

--- src/components/ImageEnlarger.js
+++ src/components/ImageEnlarger.js
@@ -1,26 +1,38 @@
 import React, { useState } from 'react';
+import './ImageWithText.css';
 
 export default function ImageEnlarger({ src, alt }) {
   const [isEnlarged, setIsEnlarged] = useState(false);
 
-  return(
-    <div className="image-container">
-      <img
-        src={src}
-        style={{ width: '100px', height: '100px' }}
-        onClick={() => setIsEnlarged(true)}
-        alt={alt}
-      />
-      {isEnlarged && (
-        <div style={{ position: 'fixed', top: 0, left: 0, right: 0, bottom: 0, background: 'rgba(0, 0, 0, 0.7)' }}>
-          <img
-            src={src}
-            style={{ position: 'absolute', top: '50%', left: '50%', transform: 'translate(-50%, -50%)', maxWidth: '80%', maxHeight: '80%' }}
-            onClick={() => setIsEnlarged(false)}
-            alt={alt}
-          />
-        </div>
-      )}
-    </div>
+  return (
+    <div class="flex w-1/3 flex-wrap">
+      <div class="w-full p-1 md:p-2">
+        <img
+          alt="gallery"
+          class="block h-full w-full rounded-lg object-cover object-center"
+          onClick={() => setIsEnlarged(true)}
+          src={src}
+        />
+      </div>
+        {isEnlarged && (
+          <div style={{ position: 'fixed', top: 0, left: 0, right: 0, bottom: 0, zIndex: 999, background: 'rgba(0, 0, 0, 0.7)' }}>
+            <img
+              src={src}
+              style={{
+                position: 'relative',
+                top: '50%',
+                left: '50%',
+                transform: 'translate(-50%, -50%)',
+                maxWidth: 'none',
+                maxHeight: 'none',
+                objectFit: 'contain',
+              }}
+              onClick={() => setIsEnlarged(false)}
+              alt="gallery"
+            />
+            <p className="text">{alt}</p>
+          </div>
+        )}
+      </div>
   );
 }
--- src/components/RandomPost.js
+++ src/components/RandomPost.js
@@ -3,7 +3,8 @@ import axios from 'axios';
 import CategoryGroup from './CategoryGroup';
 import ImageEnlarger from './ImageEnlarger';
 import styled from 'styled-components';
-import "../Gallery.css";
+import '../Gallery.css';
+import * as te from 'tw-elements';
 
 const Button = styled.button`
   background-color: black;
@@ -37,7 +38,7 @@ export default function RandomPost(props) {
         const listItems = data.data.children.map((post) => (
           <ImageEnlarger src={post.data.url} alt={post.data.title} width={700} height={700} key={post.data.id} />
         ));
-  
+
         setPosts(listItems);
       }
     }
@@ -54,38 +55,96 @@ export default function RandomPost(props) {
       <h1>
         {category} Posts in <i>r/{subreddit}</i>
       </h1>
-    <div className="Categories">
-      <ButtonToggle type="button" className="btn toggle-btn" name='best' aria-pressed={props.isPressed} onClick={() => setCategory('best')}>
-        <span className="visually-hidden">Best posts</span>
-      </ButtonToggle>
-      <ButtonToggle type="button" className="btn toggle-btn" name={props.name} aria-pressed={props.isPressed} onClick={() => setCategory('hot')}>
-        <span className="visually-hidden">Hot posts</span>
-      </ButtonToggle>
-      <ButtonToggle type="button" className="btn toggle-btn" name={props.name} aria-pressed={props.isPressed} onClick={() => setCategory('new')}>
-        <span className="visually-hidden">New posts</span>
-      </ButtonToggle>
-      <ButtonToggle type="button" className="btn toggle-btn" name={props.name} aria-pressed={props.isPressed} onClick={() => setCategory('top')}>
-        <span className="visually-hidden">Top posts</span>
-      </ButtonToggle>
-      <ButtonToggle type="button" className="btn toggle-btn" name={props.name} aria-pressed={props.isPressed} onClick={() => setCategory('rising')}>
-        <span className="visually-hidden">Rising posts</span>
-      </ButtonToggle>
-    </div>
-    <div className="Subreddits">
-      <ButtonToggle type="button" className="btn toggle-btn" name={props.name} aria-pressed={props.isPressed} onClick={() => setSubreddit('dankmemes')}>
-        <span className="visually-hidden">dankmemes</span>
-      </ButtonToggle>
-      <ButtonToggle type="button" className="btn toggle-btn" name={props.name} aria-pressed={props.isPressed} onClick={() => setSubreddit('wallpapers')}>
-        <span className="visually-hidden">wallpapers</span>
-      </ButtonToggle>
-      <ButtonToggle type="button" className="btn toggle-btn" name={props.name} aria-pressed={props.isPressed} onClick={() => setSubreddit('dogpictures')}>
-        <span className="visually-hidden">dogpictures</span>
-      </ButtonToggle>
-      <ButtonToggle type="button" className="btn toggle-btn" name={props.name} aria-pressed={props.isPressed} onClick={() => setSubreddit('EarthPorn')}>
-        <span className="visually-hidden">EarthPorn</span>
-      </ButtonToggle>
-    </div>
-      <div className="gallery">{posts}</div>
+      <div className="Categories">
+        <ButtonToggle
+          type="button"
+          className="btn toggle-btn"
+          name="best"
+          aria-pressed={props.isPressed}
+          onClick={() => setCategory('best')}
+        >
+          <span className="visually-hidden">Best posts</span>
+        </ButtonToggle>
+        <ButtonToggle
+          type="button"
+          className="btn toggle-btn"
+          name={props.name}
+          aria-pressed={props.isPressed}
+          onClick={() => setCategory('hot')}
+        >
+          <span className="visually-hidden">Hot posts</span>
+        </ButtonToggle>
+        <ButtonToggle
+          type="button"
+          className="btn toggle-btn"
+          name={props.name}
+          aria-pressed={props.isPressed}
+          onClick={() => setCategory('new')}
+        >
+          <span className="visually-hidden">New posts</span>
+        </ButtonToggle>
+        <ButtonToggle
+          type="button"
+          className="btn toggle-btn"
+          name={props.name}
+          aria-pressed={props.isPressed}
+          onClick={() => setCategory('top')}
+        >
+          <span className="visually-hidden">Top posts</span>
+        </ButtonToggle>
+        <ButtonToggle
+          type="button"
+          className="btn toggle-btn"
+          name={props.name}
+          aria-pressed={props.isPressed}
+          onClick={() => setCategory('rising')}
+        >
+          <span className="visually-hidden">Rising posts</span>
+        </ButtonToggle>
+      </div>
+      <div className="Subreddits">
+        <ButtonToggle
+          type="button"
+          className="btn toggle-btn"
+          name={props.name}
+          aria-pressed={props.isPressed}
+          onClick={() => setSubreddit('dankmemes')}
+        >
+          <span className="visually-hidden">dankmemes</span>
+        </ButtonToggle>
+        <ButtonToggle
+          type="button"
+          className="btn toggle-btn"
+          name={props.name}
+          aria-pressed={props.isPressed}
+          onClick={() => setSubreddit('wallpapers')}
+        >
+          <span className="visually-hidden">wallpapers</span>
+        </ButtonToggle>
+        <ButtonToggle
+          type="button"
+          className="btn toggle-btn"
+          name={props.name}
+          aria-pressed={props.isPressed}
+          onClick={() => setSubreddit('dogpictures')}
+        >
+          <span className="visually-hidden">dogpictures</span>
+        </ButtonToggle>
+        <ButtonToggle
+          type="button"
+          className="btn toggle-btn"
+          name={props.name}
+          aria-pressed={props.isPressed}
+          onClick={() => setSubreddit('EarthPorn')}
+        >
+          <span className="visually-hidden">EarthPorn</span>
+        </ButtonToggle>
+      </div>
+      <section class="overflow-hidden text-neutral-700">
+        <div class="container mx-auto px-5 py-2 lg:px-32 lg:pt-12">
+          <div class="-m-1 flex flex-wrap md:-m-2">{posts}</div>
+        </div>
+      </section>
     </div>
   );
 }
